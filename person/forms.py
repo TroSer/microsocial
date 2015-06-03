1d79c217e00b1f1539a387aa23318b66ef76e6fc
Sergii Tronko <troser@bigmir.net>
2015-06-04 01:07:57 +0300

    Added person edit page logic

--- person/forms.py
+++ person/forms.py
@@ -1,4 +1,8 @@
+# coding=utf-8
 from django import forms
+from django.contrib.auth.forms import PasswordChangeForm
+from django.core import validators
+from django.utils.translation import ugettext, ugettext_lazy as _
 from microsocial.forms import BootstrapFormMixin
 from person.models import User
 
@@ -6,8 +10,48 @@ from person.models import User
 class PersonForm(forms.ModelForm, BootstrapFormMixin):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('avatar', 'first_name', 'last_name', 'gender', 'birth_date', 'city', 'job', 'about_me', 'interests')
 
     def __init__(self, *args, **kwargs):
         super(PersonForm, self).__init__(*args, **kwargs)
         BootstrapFormMixin.__init__(self)
+        self.fields['birth_date'].widget.attrs['placeholder'] = ugettext(u'Введите дату в формате ГГГГ-ММ-ДД')
+
+
+class UserPasswordChangeForm(PasswordChangeForm, BootstrapFormMixin):
+    def __init__(self, *args, **kwargs):
+        super(UserPasswordChangeForm, self).__init__(*args, **kwargs)
+        BootstrapFormMixin.__init__(self)
+        for field_name in ('old_password', 'new_password1', 'new_password2'):
+            self.fields[field_name] = self.fields.pop(field_name)
+            if field_name != 'old_password':
+                self.fields[field_name].validators.extend([validators.MinLengthValidator(6),
+                                                           validators.MaxLengthValidator(40)])
+
+
+class UserEmailChangeForm(forms.Form, BootstrapFormMixin):
+    new_email = forms.EmailField(max_length=75, label=_(u'новый email'))
+    password = forms.CharField(label=_(u'текущий пароль'), widget=forms.PasswordInput())
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super(UserEmailChangeForm, self).__init__(*args, **kwargs)
+        BootstrapFormMixin.__init__(self)
+
+    def clean_new_email(self):
+        new_email = self.cleaned_data['new_email'].strip()
+        if User.objects.filter(email=new_email).exclude(pk=self.user.pk).exists():
+            raise forms.ValidationError(_(u'Email используется'))
+        return new_email
+
+    def clean_password(self):
+        password = self.cleaned_data['password']
+        if not self.user.check_password(password):
+            raise forms.ValidationError(_(u'Введен неправильный пароль.'))
+        return password
+
+    def save(self, commit=True):
+        self.user.email = self.cleaned_data['new_email']
+        if commit:
+            self.user.save()
+        return self.user
--- person/views.py
+++ person/views.py
@@ -1,10 +1,13 @@
 # coding=utf-8
 from django.contrib import messages
+from django.contrib.auth import BACKEND_SESSION_KEY, login
+from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404, redirect
+from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext
 from django.views.generic import TemplateView
-from person.forms import PersonForm
+from person.forms import PersonForm, UserPasswordChangeForm, UserEmailChangeForm
 from person.models import User
 
 
@@ -15,23 +18,40 @@ class PersonProfileView(TemplateView):
 class PersonEditView(TemplateView):
     template_name = 'person/profile_edit.html'
 
+    @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
-        self.instance = get_object_or_404(User, pk=request.user.id)
-
-        self.form = PersonForm(request.POST or None, instance=self.instance)
+        action = request.POST.get('action')
+        self.profile_form = PersonForm(
+            (request.POST if action == 'profile' else None),
+            (request.FILES if action == 'profile' else None),
+            prefix='profile', instance=request.user
+        )
+        self.password_form = UserPasswordChangeForm(request.user, (request.POST if action == 'password' else None),
+                                                    prefix='password')
+        self.email_form = UserEmailChangeForm(request.user, (request.POST if action == 'email' else None),
+                                                    prefix='email')
         return super(PersonEditView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(PersonEditView, self).get_context_data(**kwargs)
-        context['form'] = self.form
+        context['profile_form'] = self.profile_form
+        context['password_form'] = self.password_form
+        context['email_form'] = self.email_form
         return context
 
     def post(self, request, *args, **kwargs):
-        if self.form.is_valid():
-            saved_instance = self.form.save()
-            if not self.instance:
-                messages.success(request, ugettext(u'Профиль успешно создан.'))
-            else:
-                messages.success(request, ugettext(u'Профиль успешно сохранен.'))
-            return redirect('profile', person_id=saved_instance.pk)
+        if self.profile_form.is_valid():
+            self.profile_form.save()
+            messages.success(request, ugettext(u'Профиль успешно сохранен.'))
+            return redirect(request.path)
+        elif self.password_form.is_valid():
+            self.password_form.save()
+            request.user.backend = request.session[BACKEND_SESSION_KEY]
+            login(request, request.user)
+            messages.success(request, ugettext(u'Пароль успешно сохранен.'))
+            return redirect(request.path)
+        elif self.email_form.is_valid():
+            self.email_form.save()
+            messages.success(request, ugettext(u'Email успешно сохранен.'))
+            return redirect(request.path)
         return self.get(request, *args, **kwargs)
