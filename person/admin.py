7af2d37653ed5f9a622edb6b3ac2ca82ea42b325
Sergii Tronko <troser@bigmir.net>
2015-06-09 22:04:52 +0300

    friendship apllication added

--- person/admin.py
+++ person/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from person.models import User, UserWallPost
+from person.models import User, UserWallPost, FriendInvite
 
 admin.site.register(User)
+admin.site.register(FriendInvite)
 admin.site.register(UserWallPost)
 
